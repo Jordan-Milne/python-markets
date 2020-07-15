111f1efecfd440a8a31c4d337bbce2ec34e938f1
Jordan <jordan.milne2@gmail.com>
2020-07-15 17:11:28 -0400

    comparison chart

--- title.py
+++ title.py
@@ -2,14 +2,6 @@ import pandas as pd
 import plotly.express as px
 import yfinance as yf
 
-# NASDAQ = yf.Ticker("^IXIC").history(period="max")
-# NASDAQ['date'] = NASDAQ.index
-# NASDAQ = NASDAQ.reset_index(drop=True)
-#
-# name = yf.Ticker(symbol)
-# data = name.history(period="max")
-# data['date'] = data.index
-# data = data.reset_index(drop=True)
 
 def chart(symbol, date_range):
     name = yf.Ticker(symbol)
@@ -24,3 +16,32 @@ def chart(symbol, date_range):
 
 
 chart('AMZN','2y')
+
+def comparison(symbol,symbol2, date_range):
+    name = yf.Ticker(symbol)
+    data = name.history(period=date_range)
+    data['date'] = data.index
+    data = data.reset_index(drop=True)
+    data['name'] = str(symbol)
+    data['percent'] = (data['Close']/data['Close'].iloc[0])-1
+
+    stock2 = yf.Ticker(str(symbol2)).history(period=date_range)
+    stock2['date'] = stock2.index
+    stock2 = stock2.reset_index(drop=True)
+    stock2['name'] = str(symbol2)
+    stock2['percent'] = (stock2['Close']/stock2['Close'].iloc[0])-1
+
+    df = pd.concat([data,stock2])
+
+
+    fig = px.scatter(df, x='date', y='percent', color='name')
+    fig.update_xaxes(
+        rangebreaks=[
+            dict(bounds=["sat", "mon"]), #hide weekends
+        ]
+    )
+
+    fig.show()
+
+
+comparison('AMZN','AAPL', '1mo')
