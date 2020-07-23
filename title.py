0a9f29562bdc49bc718b1d5a22e9086fe01788dc
Jordan <jordan.milne2@gmail.com>
2020-07-23 15:37:58 -0400

    learning dash

--- title.py
+++ title.py
@@ -1,6 +1,32 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 import pandas as pd
-import plotly.express as px
 import yfinance as yf
+
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+html.H1('Hello Dash!'),
+html.Div('Dash: Web Dashboards with python'),
+dcc.Graph(id='example',
+            figure={'data':[
+            {'x':[1,2,3],'y':[4,1,2],'type':'bar','name':'SF'},
+            {'x':[1,2,3],'y':[2,4,5],'type':'bar','name':'NYC'}
+            ],
+                    'layout':{
+                    'title':'BAR PLOTS!'
+                    }})
+])
+
+if __name__ == '__main__':
+    app.run_server()
+
+
+
+
+
 
 
 def chart(symbol, date_range):
