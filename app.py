56f4ab58431448e7700236ad2bf2c46f1d69bb1c
Jordan <jordan.milne2@gmail.com>
2020-09-08 11:56:39 -0400

    adding tabs

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ app = dash.Dash()
 app.layout = html.Div([
     html.H1('Stock Analyzer Dashboard'),
     dcc.Markdown(''' --- '''),
-    html.H2('Chart'),
+    dcc.Tabs(id='tabs', children=[
+        dcc.Tab(label='Chart', value='tab_chart', children=[
     html.Div([html.H3('Enter a stock symbol:', style={'paddingRight':'30px'}),
     dcc.Dropdown(
         id='my_ticker_symbol',
@@ -56,8 +57,35 @@ app.layout = html.Div([
                 {'x': [1,2], 'y': [3,1]}
             ]
         }
+    )
+    ]),
+    dcc.Tab(label='Comparison', value='tab_comp', children=[
+    html.Div([html.H3('Enter a stock symbol:', style={'paddingRight':'30px'}),
+    dcc.Dropdown(
+        id='comp_ticker_symbol',
+        options=tickerz,
+        value=['TSLA'],
+        multi=True
+    )], style={'display':'inline-block', 'verticalAlign':'top','width':'30%'}),
+    html.Div([
+        html.H3('Select start and end dates:'),
+        dcc.DatePickerRange(
+            id='comp_date_picker',
+            min_date_allowed = datetime(1950, 1, 1),
+            max_date_allowed = datetime.today(),
+            start_date = datetime(2018, 1, 1),
+            end_date = datetime.today()
+        )
+    ], style={'display':'inline-block'}),
+    html.Div([
+            html.Button(id='comp_submit-button',
+            n_clicks=0,
+            children='Submit',
+            style={'fontSize':24,'marginLeft':'30px'}
+
+    )],
+    style={'display':'inline-block'}
     ),
-    dcc.Markdown(''' --- '''),
     dcc.Graph(
         id='comp_chart',
         figure={
@@ -65,8 +93,11 @@ app.layout = html.Div([
                 {'x': [1,2], 'y': [3,1]}
             ]
         }
-    ),
+    )
+    ]),
 ])
+])
+
 @app.callback(
     Output('inter_chart', 'figure'),
     [Input('submit-button','n_clicks')],
@@ -95,10 +126,10 @@ def update_graph(n_clicks,stock_ticker, start_date, end_date):
 
 @app.callback(
     Output('comp_chart', 'figure'),
-    [Input('submit-button','n_clicks')],
-    [State('my_ticker_symbol', 'value'),
-    State('my_date_picker', 'start_date'),
-    State('my_date_picker', 'end_date')])
+    [Input('comp_submit-button','n_clicks')],
+    [State('comp_ticker_symbol', 'value'),
+    State('comp_date_picker', 'start_date'),
+    State('comp_date_picker', 'end_date')])
 def update_comp_graph(n_clicks,stock_ticker, start_date, end_date):
     start = datetime.strptime(start_date[:10], '%Y-%m-%d')
     end = datetime.strptime(end_date[:10], '%Y-%m-%d')
